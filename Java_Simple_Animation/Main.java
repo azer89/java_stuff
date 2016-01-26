9ad389f65fda2dadbae6e510b8284144bb1e6aa1
azer89 <radhitya@uwaterloo.ca>
2016-01-26 08:59:17 -0500

    added layout

--- Java_Simple_Animation/Main.java
+++ Java_Simple_Animation/Main.java
@@ -52,25 +52,21 @@ public class Main {
 		frame.setBounds(100, 100, 1024, 768);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
+		JPanel my_panel = new JPanel();
+		frame.getContentPane().add(my_panel, BorderLayout.CENTER);
+		my_panel.setLayout(new BorderLayout());
 		
+		Canvas my_canvas = new Canvas();		
+		my_panel.add(my_canvas, BorderLayout.CENTER);
+
+		//my_canvas.setBounds(163, 7, 859, 674);
+		//my_panel.add(my_canvas);		
+
+		/*
 		JLabel lblNewLabel = new JLabel("Canadaball");
 		lblNewLabel.setBounds(34, 7, 117, 15);
 		panel.add(lblNewLabel);
-		
-		/*JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.GRAY);
-		panel_1.setBounds(163, 7, 859, 674);
-		panel.add(panel_1);
-		*/
-		
-		Canvas panel_1 = new Canvas();
-		panel_1.setBackground(Color.GRAY);
-		panel_1.setBounds(163, 7, 859, 674);
-		panel.add(panel_1);
-		
+
 		JButton btnButton = new JButton("Button");
 		btnButton.setBounds(34, 95, 117, 25);
 		panel.add(btnButton);
@@ -78,8 +74,6 @@ public class Main {
 		JLabel lblRidesAMoose = new JLabel("I'm SORRY !!!");
 		lblRidesAMoose.setBounds(34, 54, 117, 15);
 		panel.add(lblRidesAMoose);
-		
-		//Canvas canvas = new Canvas();
-		//panel_1.add(canvas);
+		*/
 	}
 }
